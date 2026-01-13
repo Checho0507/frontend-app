24fe3bed3a78f130b26f5e5a7f2969fa2feef95e
Checho0507 <checho.mlj@gmail.com>
2026-01-13 17:48:26 -0500

    10 K max

--- src/pages/caraosello.tsx
+++ src/pages/caraosello.tsx
@@ -297,7 +297,7 @@ export default function CaraSello() {
         setUsuario(null);
     };
 
-    const valoresApuesta = [100, 200, 500, 1000, 2000, 5000];
+    const valoresApuesta = [100, 200, 500, 1000, 2000, 5000, 10000];
 
     if (!usuario) {
         return (
